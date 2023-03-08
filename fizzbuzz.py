49039612e6dea2956c48a30edce96cc1c5aee879
Akpowo ochepa <manny.babida@gmail.com>
2023-03-08 16:25:05 -0600

    efficiency of the program

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,14 @@
-Getting the Program to work
+2. Improving the efficiency of the program
 
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
+    if i % 3 == 0:
+        if i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
     else:
         print(i)
+
 
