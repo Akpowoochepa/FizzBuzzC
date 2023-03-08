a780f1d7f7e948474dc16da7a42610c9fc96678b
Akpowo ochepa <manny.babida@gmail.com>
2023-03-08 16:32:07 -0600

    Adding Comments to the code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,20 @@
-Getting the Program to work
+Adding comments to the code to allow the users to understand and read the 
+code better.
 
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
+# Loop through the numbers 1 to 100
+for num in range(1, 100):
+    # Check if the number is divisible by 3
+    if num % 3 == 0:
+        # Check if the number is also divisible by 5
+        if num % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print("Fizz")
+    # Check if the number is divisible by 5
+    elif num % 5 == 0:
         print("Buzz")
+    # If the number is not divisible by 3 or 5, print the number itself
     else:
-        print(i)
+        print(num)
+
 
